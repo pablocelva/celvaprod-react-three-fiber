6722b41db89c7a559e00858f70ed140befeecfdf
Pablo Lambert <pablomartin.lambert@gmail.com>
2025-10-02 22:05:34 -0300

    fix base vite para netlify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/celvaprod-react-three-fiber/',
+  //base: '/celvaprod-react-three-fiber/',
 })
